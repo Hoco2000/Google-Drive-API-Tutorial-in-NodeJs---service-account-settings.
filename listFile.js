9e556abee3a721dda45e29e9d93675f91cfe8d6d
HorizonCoding <38108303+HorizonCoding@users.noreply.github.com>
2018-06-04 22:53:43 +0200

    Update listFile.js

--- listFile.js
+++ listFile.js
@@ -1,20 +1,22 @@
 // list file in speciifcs folder
-var parents=  "enter here the folder you target"
+var parents = "enter here the folder you target"
 drive.files.list({
 
-    auth: jwtClient,
-    pageSize: 10,
-    q: "'" + parents + "' in parents and trashed=false",
-    fields: 'files(id, name)',
-  }, (err, {data}) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const files = data.files;
-    if (files.length) {
-      console.log('Files:');
-      files.map((file) => {
-        console.log(`${file.name} (${file.id})`);
-      });
-    } else {
-      console.log('No files found.');
-    }
-  });
+  auth: jwtClient,
+  pageSize: 10,
+  q: "'" + parents + "' in parents and trashed=false",
+  fields: 'files(id, name)',
+}, (err, {
+  data
+}) => {
+  if (err) return console.log('The API returned an error: ' + err);
+  const files = data.files;
+  if (files.length) {
+    console.log('Files:');
+    files.map((file) => {
+      console.log(`${file.name} (${file.id})`);
+    });
+  } else {
+    console.log('No files found.');
+  }
+});
